cfd8c2138e01f59c45fe5b1b68ab147218b59698
Deron Smith <deron.brock@gmail.com>
2018-04-26 07:16:24 -0500

    Reset scenario cap

--- Tool/functions.py
+++ Tool/functions.py
@@ -659,8 +659,8 @@ class Scenarios(object):
 
             # TODO: Split scenario into multiple loops where each loop stores the results in a database that are
             # merged after completion (reducing memory requirements), allowing for removal of the following two lines.
-            # if n == 50000:
-            #     break
+            if n == 45000:
+                break
 
             # Report progress and reset readers/writers at intervals
             if not (n + 1) % progress_interval:
